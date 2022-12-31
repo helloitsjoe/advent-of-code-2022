de7598f56cc8665eff55b49042d178a17b3ca4a4
helloitsjoe <jboyle617@gmail.com>
2022-12-30 20:12:18 -0500

    EFUGLPAP

--- go/advent09/advent09.go
+++ go/advent09/advent09.go
@@ -8,6 +8,17 @@ import (
 	"strings"
 )
 
+func getXByCycle(cycles []int, cycleNum int) int {
+	x := 1
+	for i, cycle := range cycles {
+		if i == cycleNum-1 {
+			return x
+		}
+		x += cycle
+	}
+	return x
+}
+
 func getSignalByCycle(cycles []int, cycleNum int) int {
 	x := 1
 	for i, cycle := range cycles {
@@ -34,6 +45,38 @@ func parseCycles(content []string) []int {
 	return cycles
 }
 
+func getSumOfCycles(cycles []int) int {
+	sum := 0
+	for _, cycle := range []int{20, 60, 100, 140, 180, 220} {
+		signal := getSignalByCycle(cycles, cycle)
+		fmt.Println(signal)
+		sum += signal
+	}
+	return sum
+}
+
+func createImageOnCRT(cycles []int) []string {
+	image := make([]string, 6)
+	fmt.Println(cycles)
+
+	for i := range image {
+		curr := ""
+		for j := 1; j < 41; j++ {
+			x := getXByCycle(cycles, j+(40*i))
+			h, m, t := x-1, x, x+1
+			fmt.Println(h, m, t, j, x)
+			if j-1 == h || j-1 == m || j-1 == t {
+				curr += "#"
+			} else {
+				curr += "."
+			}
+		}
+		image[i] = curr
+	}
+
+	return image
+}
+
 func Run() {
 	filePath := path.Join("..", "advent09", "input.txt")
 	bytes, _ := os.ReadFile(filePath)
@@ -41,11 +84,13 @@ func Run() {
 
 	cycles := parseCycles(content)
 
-	sum := 0
-	for _, cycle := range []int{20, 60, 100, 140, 180, 220} {
-		signal := getSignalByCycle(cycles, cycle)
-		fmt.Println(signal)
-		sum += signal
+	// Part 1
+	// sum := getSumOfCycles(cycles)
+	// fmt.Println(sum)
+
+	// Part 2
+	image := createImageOnCRT(cycles)
+	for _, line := range image {
+		fmt.Println(line)
 	}
-	fmt.Println(sum)
 }
