74663bef60d6663695f517f35fba62d99649180e
helloitsjoe <jboyle617@gmail.com>
2023-01-10 09:54:26 -0500

    test code working

--- rust/src/advent13.rs
+++ rust/src/advent13.rs
@@ -1,20 +1,46 @@
-use serde_json::Value;
+use serde_json::{
+    Value,
+    Value::{Array, Number},
+};
 use std::cmp::min;
 use std::fs;
 
-fn compare_int(a: u64, b: u64) -> bool {
-    if a == b || a > b {
-        return false;
+fn compare_int(a: u64, b: u64) -> Option<bool> {
+    // println!("int a {:?}", a);
+    // println!("int b {:?}", b);
+    if a == b {
+        return None;
     } else {
-        return true;
+        return Some(a < b);
     }
 }
 
-fn compare_arr(a: Vec<Value>, b: Vec<Value>) -> bool {
-    if a.len() < b.len() {
-        return true;
-    } else {
-        return false;
+fn compare_arr(a: Vec<Value>, b: Vec<Value>) -> Option<bool> {
+    // println!("compare_arr a {:?}", a);
+    // println!("compare_arr b {:?}", b);
+
+    for i in 0..min(a.len(), b.len()) {
+        let a_val = &a[i];
+        let b_val = &b[i];
+
+        if a_val != b_val {
+            return compare_match(a_val, b_val);
+        }
+    }
+
+    return Some(a.len() < b.len());
+}
+
+fn compare_match(val_a: &Value, val_b: &Value) -> Option<bool> {
+    return match (val_a, val_b) {
+        (Number(a), Number(b)) => compare_int(a.as_u64().unwrap(), b.as_u64().unwrap()),
+        (Array(a), Array(b)) => compare_arr(a.to_vec(), b.to_vec()),
+        (Number(_), Array(b)) => compare_arr(vec![val_a.clone()], b.to_vec()),
+        (Array(a), Number(_)) => compare_arr(a.to_vec(), vec![val_b.clone()]),
+        _ => {
+            println!("unknown");
+            return Some(false);
+        }
     };
 }
 
@@ -30,26 +56,17 @@ pub fn run() {
         .collect();
 
     for (i, pair) in pairs.iter().enumerate() {
-        let line_a: Vec<Value> = serde_json::from_str(pair[0]).unwrap();
-        let line_b: Vec<Value> = serde_json::from_str(pair[1]).unwrap();
+        let line_a: Value = serde_json::from_str(pair[0]).unwrap();
+        let line_b: Value = serde_json::from_str(pair[1]).unwrap();
 
-        for j in 0..min(line_a.len(), line_b.len()) {
-            let val_a: &Value = &line_a[j];
-            let val_b: &Value = &line_b[j];
+        // println!("====== START {}", i + 1);
 
-            let correct: bool = match (val_a, val_b) {
-                (Value::Number(a), Value::Number(b)) => {
-                    compare_int(a.as_u64().unwrap(), b.as_u64().unwrap())
-                }
-                (Value::Array(a), Value::Array(b)) => compare_arr(a.to_vec(), b.to_vec()),
-                _ => false,
-            };
-
-            if correct == true {
-                right_order_indices.push(i);
-            }
+        if let Some(true) = compare_match(&line_a, &line_b) {
+            // println!("correct");
+            right_order_indices.push(i + 1);
         }
     }
 
     println!("{:?}", right_order_indices);
+    println!("{:?}", right_order_indices.iter().sum::<usize>());
 }
