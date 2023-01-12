bf9f6c28d3a7fb96661f7f818e720352bf54224f
helloitsjoe <jboyle617@gmail.com>
2023-01-11 19:20:28 -0500

    compare strings

--- rust/src/advent13.rs
+++ rust/src/advent13.rs
@@ -1,51 +1,51 @@
-use serde_json::{
-    Value,
-    Value::{Array, Number},
-};
 use std::cmp::min;
 use std::fs;
 
-fn compare_int(a: u64, b: u64) -> Option<bool> {
-    println!("int a {:?}", a);
-    println!("int b {:?}", b);
-    if a == b {
-        return None;
-    } else {
-        return Some(a < b);
-    }
-}
-
-fn compare_arr(a: Vec<Value>, b: Vec<Value>) -> Option<bool> {
-    println!("compare_arr a {:?}", a);
-    println!("compare_arr b {:?}", b);
+fn compare_lines(a: &str, b: &str) -> bool {
+    let mut result: bool = false;
 
     for i in 0..min(a.len(), b.len()) {
-        let a_val = &a[i];
-        let b_val = &b[i];
+        let a_char: char = a.chars().nth(i).unwrap();
+        let b_char: char = b.chars().nth(i).unwrap();
 
-        if a_val != b_val {
-            return compare_match(a_val, b_val);
+        if a_char.is_digit(10) && b_char.is_digit(10) {
+            if a_char != b_char {
+                result = a_char < b_char;
+            }
+        }
+
+        if a_char == '[' && b_char.is_digit(10) {
+            let new_a_char = a.chars().nth(i + 1).unwrap();
+            if new_a_char < b_char {
+                return true;
+            } else if new_a_char < b_char {
+                return false;
+            } else if new_a_char == b_char {
+                if a.chars().nth(i + 2).unwrap() == ']' {
+                    return true;
+                }
+            }
+        }
+
+        if b_char == '[' && a_char.is_digit(10) {
+            let new_b_char = b.chars().nth(i + 1).unwrap();
+            if a_char < new_b_char {
+                return true;
+            } else if new_b_char < a_char {
+                return false;
+            } else if new_b_char == a_char {
+                if b.chars().nth(i + 2).unwrap() != ']' {
+                    return true;
+                }
+            }
+        }
+
+        if a_char == ']' && b_char != ']' {
+            return true;
         }
     }
 
-    if a.len() != b.len() {
-        return Some(a.len() < b.len());
-    }
-
-    return None;
-}
-
-fn compare_match(val_a: &Value, val_b: &Value) -> Option<bool> {
-    return match (val_a, val_b) {
-        (Number(a), Number(b)) => compare_int(a.as_u64().unwrap(), b.as_u64().unwrap()),
-        (Array(a), Array(b)) => compare_arr(a.to_vec(), b.to_vec()),
-        (Number(_), Array(b)) => compare_arr(vec![val_a.clone()], b.to_vec()),
-        (Array(a), Number(_)) => compare_arr(a.to_vec(), vec![val_b.clone()]),
-        _ => {
-            println!("unknown");
-            return Some(false);
-        }
-    };
+    return result;
 }
 
 pub fn run() {
@@ -60,12 +60,14 @@ pub fn run() {
         .collect();
 
     for (i, pair) in pairs.iter().enumerate() {
-        let line_a: Value = serde_json::from_str(pair[0]).unwrap();
-        let line_b: Value = serde_json::from_str(pair[1]).unwrap();
+        let line_a = pair[0];
+        let line_b = pair[1];
 
         println!("====== START {}", i + 1);
+        println!("A {:?}", line_a);
+        println!("B {:?}", line_b);
 
-        if let Some(true) = compare_match(&line_a, &line_b) {
+        if compare_lines(line_a, line_b) == true {
             println!("correct");
             right_order_indices.push(i + 1);
         }
